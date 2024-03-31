eac0bf6fe233832e06789fac7617e3e7aa84d8e9
kalpanakulkarni085 <133846584+kalpanakulkarni085@users.noreply.github.com>
2024-03-31 22:02:10 +0530

    file upload

--- Kalpana/Employee management system/Employee management system/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Employee_management_system.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
